e81c2efc311eaf7a7000a3b555c31086ca1bf823
Weiren Gan <weirengan@WeirendeMacBook-Pro.local>
2025-04-25 22:34:09 +0800

    🌲feat(dev): init project

---
+++ .commitlintrc.js
@@ -0,0 +1,20 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // 新功能
+        "fix", // Bug 修复
+        "docs", // 文档更新
+        "style", // 代码格式（不影响功能）
+        "refactor", // 重构（非功能或 Bug 修复）
+        "test", // 测试相关
+        "chore", // 构建或辅助工具变动
+        "revert", // 回滚提交
+      ],
+    ],
+    "scope-empty": [2, "never"], // scope 不能为空
+  },
+};
---
+++ src/types/global.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+
+declare namespace GlobalType {
+  interface ComponentITF {
+    className?: string;
+    style?: React.CSSProperties;
+    children?: React.ReactNode;
+  }
+}
