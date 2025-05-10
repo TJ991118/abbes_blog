f9ce9cc5ff87701af08dbe185573735b333f17fb
GWR <tomjerry991118@gmail.com>
2025-05-10 10:53:44 +0800

    lint(dev): fix typescript lint

--- src/stores/slice/user.ts
+++ src/stores/slice/user.ts
@@ -1,40 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { profile } from "console";
 
 /* 
   DESC Types for user slice
 */
 type AccountProfileType = {
-  user_id: string
-  lang: string
-  bind_weixin: string
-  bind_google: string
-  roles: string[]
-  permission: string[]
-  notice_setting: string
-  phone_verified: boolean
-  email_verified: boolean
-}
+  user_id: string;
+  lang: string;
+  bind_weixin: string;
+  bind_google: string;
+  roles: string[];
+  permission: string[];
+  notice_setting: string;
+  phone_verified: boolean;
+  email_verified: boolean;
+};
 
 type UserProfileType = {
-  username: string
-  nickname: string
-  gender: string
-  avatar: string
-  phone: string
-  email: string
-}
+  username: string;
+  nickname: string;
+  gender: string;
+  avatar: string;
+  phone: string;
+  email: string;
+};
 
 const initialState = {
   account: {} as AccountProfileType,
-  profile: {} as UserProfileType
-}
+  profile: {} as UserProfileType,
+};
 
 const userSlice = createSlice({
   name: "user",
   initialState: initialState,
-  reducers: {
-  }
-})
+  reducers: {},
+});
 
-export default userSlice
+export default userSlice;
