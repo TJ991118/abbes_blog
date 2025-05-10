b70ce6b1b21f40f06d3b25be2202a3add16d4dfa
GWR <tomjerry991118@gmail.com>
2025-05-10 15:21:01 +0800

    🐳 chore(custom): config the commit message with emoji

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,28 +1,6 @@
 export default {
-  extends: ["@commitlint/config-conventional"],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(\p{Emoji_Presentation}?)\s?(\w*)(\(\w*\))?:\s(.*)$/u,
-      headerCorrespondence: ["emoji", "type", "scope", "subject"],
-    },
-  },
-  // 🌲 🛠 📝 🧲 📦 📜 🔖 📌
+  extends: ["git-commit-emoji"],
   rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat", // New Function
-        "fix", // Bug Fix
-        "docs", // Updated Document
-        "style", // Code Style
-        "refactor", // Refactor
-        "chore", // Build
-        "revert", // Revert Commit
-        "lint", //Lint update
-      ],
-    ],
-    "scope-empty": [2, "never"],
     "type-empty": [2, "never"],
     "subject-empty": [2, "never"],
   },
